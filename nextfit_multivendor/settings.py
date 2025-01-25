fa742d2af7b3f43eb7972f1b77801cc8fcd89e90
shariful104 <sharifulpc04@gmail.com>
2025-01-25 11:05:01 +0600

    fix host and debug

--- nextfit_multivendor/settings.py
+++ nextfit_multivendor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mgd49(3$-97i+3h@gfg%+$5mt#vw=c0iytzg-2y%is!i2wj0vl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
